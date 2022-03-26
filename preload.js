34247370cdc6b2adb7740e13db427b4f3aab034f
jasonjsyuan <jasonjsyuan@tencent.com>
2022-03-26 10:54:10 +0800

    add search

--- preload.js
+++ preload.js
@@ -7,18 +7,25 @@ function gen_idkey_url(s) {
     const idkey_key = results[2] ? results[2] : ''
     result = []
     // 有key则展示一个idkey监控
-    if(idkey_key){
+    if (idkey_key) {
         result.push({
-                    title: results[0],
-                    description: results[0],
-                    url: `https://wego.weixin.oa.com/wego/weviewweb/idkey-view?type=server&idkey_id=${idkey_id}&idkey_key=${idkey_key}&idc=all&granularity=-1&diff_days=1&dbflag=1&ip=&count=1&datatype=999`
-            })
+            title: 'IdKey Monitor',
+            description: 'Monitor for ' + results[0],
+            url: `https://wego.weixin.oa.com/wego/weviewweb/idkey-view?type=server&idkey_id=${idkey_id}&idkey_key=${idkey_key}&idc=all&granularity=-1&diff_days=1&dbflag=1&ip=&count=1&datatype=999`
+        })
     }
+    // 展示id的全监控
     result.push({
-                    title: idkey_id,
-                    description: idkey_id,
-                    url: `https://wego.weixin.oa.com/wego/weviewweb/idkey-view?type=server&idkey_id=${idkey_id}&idkey_key=&idc=all&granularity=-1&diff_days=1&dbflag=1&ip=&count=1&datatype=999`
-            })
+        title: 'Id Monitor',
+        description: 'Monitor for ' + idkey_id,
+        url: `https://wego.weixin.oa.com/wego/weviewweb/idkey-view?type=server&idkey_id=${idkey_id}&idkey_key=&idc=all&granularity=-1&diff_days=1&dbflag=1&ip=&count=1&datatype=999`
+    })
+    // 展示到codesearch的搜索
+    result.push({
+        title: 'Search',
+        description: 'Search for ' + results[0],
+        url: 'http://mmtest.oa.com/codesearch/search?q="' + encodeURIComponent(results[0]) + '"&defs=&refs=&path=&hist=&type=&project=ac&project=antispam&project=appplatform&project=astragateway&project=astragateway2&project=basic&project=bt&project=comm2&project=dry-basic&project=dry-platform&project=ilink&project=ilinkapplogin&project=ilinkcore&project=ilinkgateway&project=ilinkvoip&project=ilinkvoipgateway&project=imunion&project=imuniongateway&project=kvstore&project=micromsg&project=micromsg2&project=mm3rd&project=mm3rd2&project=mmad&project=mmadgateway&project=mmad_dataprocess_spark&project=mmaegateway&project=mmappengine&project=mmbdgateway&project=mmbiz&project=mmbiz2&project=mmbizgateway&project=mmbizgateway2&project=mmbiz_666&project=mmcomm&project=mmcomm2&project=mmcontrib&project=mmdatagateway&project=mmemoticonopen&project=mmfddatagateway&project=mmfindergateway&project=mmfindermcngateway&project=mmgamecenter&project=mmgamegateway&project=mmgateway&project=mmgog2&project=mmgoggateway&project=mmgoggateway2&project=mmiot&project=mmiotgateway&project=mmlife&project=mmlifegateway&project=mmocbiz&project=mmocbizgateway&project=mmoss&project=mmossgateway&project=mmpay&project=mmpaygateway&project=mmpaygitgateway&project=mmpay_git&project=mmpbkvclient&project=mmproto&project=mmrecgateway&project=mmsearch&project=mmsearch2&project=mmsearchgateway&project=mmsearchgateway2&project=mmspamgateway&project=mmsvrkitagent&project=mmtenpay&project=mmtenpay_bazel&project=mmtestservice&project=mmux&project=mmuxgateway&project=oss&project=phxgateway&project=phxpaxos&project=phxqueue&project=phxqueueapp&project=phxrpc&project=phxsql&project=platform&project=proxy&project=wego&project=wegraphdb&project=wqueue&project=wrpc&project=wxmesh&project=xmail&project=yard&project=yard-online'
+    })
     return result
 }
 
